835baae846b6eb939e3dbdc081e87a3099fa085b
Caio Armestrong Fernandes Costa Jardim <caiofernandes243@gmail.com>
2021-05-27 21:10:08 -0300

    testing cors

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const ItemRoute = require('./Routes/Item.route');
 const createErros = require('http-errors');
 const dotenv = require('dotenv').config();
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000
