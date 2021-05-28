705fde1b14671c16e5b93ef001dbf8755319300d
Caio Armestrong Fernandes Costa Jardim <caiofernandes243@gmail.com>
2021-05-27 21:55:09 -0300

    add origin to cors

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 const cors = require('cors');
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000
